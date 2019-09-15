84be1841b305c9a2048c51b278263da792e6c8f8
mateusz_kalwaj <tynozobacz@gmail.com>
2019-09-15 16:38:29 +0200

    player statistic added

---
+++ Assets/Scripts/CharStats.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharStats : MonoBehaviour {
+
+    public string charName;
+    public int playerLevel = 1;
+    public int currentXP;
+
+    public int currentHP;
+    public int maxHP = 100;
+    public int currentMP;
+    public int maxMP = 30;
+    public int strength;
+    public int defence;
+    public int wpnPwr;
+    public int armorPwr;
+    public string equippedWpn;
+    public string equippedArmor;
+    public Sprite charImage;
+
+    // Start is called before the first frame update
+    void Start() {
+
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        
+    }
+}
